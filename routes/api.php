cf4626466de8a05a05eacc75cbfaa56351964733
Max <leonov777max@yandex.ru>
2019-05-21 00:46:40 +1000

    Resource add News

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+|----------------
+|   DGH   |  test
+|----------------
+|   v1 Api. for Domotekhnika.ru
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function (){
+    Route::apiResource('/news', 'NewsController');
 });
 
-
-Route::apiResource('/news', 'NewsController');
